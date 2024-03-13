f22070ce4d61b62ac90b9106531e169a9b14c5bc
Okan Çifçi <161123817+okancfc@users.noreply.github.com>
2024-03-13 21:42:10 +0300

    Initial Commit

---
+++ DrinkMe/DrinkMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkMeApp.swift
+//  DrinkMe
+//
+//  Created by Okan Çifçi on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct DrinkMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
